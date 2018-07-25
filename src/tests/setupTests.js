fab58e96f0ac86e9993916e9e6cfbf25733185b0
mrybak834 <mrybak834@gmail.com>
2018-07-25 10:47:05 -0500

    Tests

---
+++ src/tests/setupTests.js
@@ -0,0 +1,15 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+// import DotEnv from 'dotenv';
+
+/**
+ * Configure enzyme to work with react 16
+ */
+Enzyme.configure({
+    adapter: new Adapter()
+});
+
+/**
+ * Use test db if in a test environment
+ */
+// DotEnv.config({ path: '.env.test' });
