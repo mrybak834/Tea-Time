2ec20a45305498ac43e32dee27423a4283ca5c83
mrybak834 <mrybak834@gmail.com>
2018-07-25 04:39:52 -0500

    Added uuid

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import uuid from 'uuid';
 
 class App extends Component {
   render() {
@@ -12,7 +13,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          <a href="/api/">Yo</a>
+          <a href="/api/">{uuid()}</a>
         </p>
       </div>
     );
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const uuid = require('uuid');
 
 const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 app.get('/api/', (req, res) => {
-    res.json("Working!");
+    res.json(uuid());
 });
 
 
