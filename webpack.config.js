e9e6e79938b08ea8f5d2c5761a2e317108319aa8
mrybak834 <mrybak834@gmail.com>
2018-07-25 10:18:19 -0500

    NODE_ENV setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+// Specify if we are in dev or prod or test
+// Production on Heroku, Test in testing, development in dev
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+
 module.exports = (env) => {
 
     const isProduction = (env === 'production');
+    /**
+     * TODO: Replace isProduction with NODE_ENV
+     * DO NOT replace build scripts, since heroku will set it's own NODE_ENV
+     */
+    console.log('isProduction', isProduction);
+    console.log('NODE_ENV', process.env.NODE_ENV);
 
     return {
         entry: ['babel-polyfill', './src/app.js'],
