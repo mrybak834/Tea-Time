a026774f9d64c2992ab8bc01d101be1d5883d8f1
mrybak834 <mrybak834@gmail.com>
2018-07-25 09:05:37 -0500

    MVP: React + Webpack

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const jsx = (
+    <div>
+        <h1>Welcome!</h1>
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
-module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
+const path = require('path');
+
+module.exports = () => {
+    return {
+        entry: ['babel-polyfill', './src/app.js'],
+        output: {
+            path: path.join(__dirname, './public/', 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
                 }
-            }
-        ]
-    }
+            ]
+        },
+        mode: 'development',
+        devtool: 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, './public/'),
+            historyApiFallback: true,
+            publicPath: '/dist/'
+        }
+    };
 };
